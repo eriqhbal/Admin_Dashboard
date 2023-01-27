0360e57f01b528fff34d5aca9991855c7aaa0912
Zuleriqhbal <eriqhbal@gmail.com>
2023-01-27 12:02:03 +0700

    Finished Customers Page Component

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import { Header } from '../components'
+
+import { ColumnsDirective, ColumnDirective, Inject, GridComponent, Page, Selection, Edit, Toolbar, Sort, Filter} from '@syncfusion/ej2-react-grids';
+
+import { customersData, customersGrid } from '../data/dummy';
+
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className="m-2 mt-16 p-2 md:m-10 md:p-10 bg-white rounded-3xl">
+      <Header category={'Pages'} title='Customers'/>
+      
+      <GridComponent dataSource={customersData} allowPaging allowSorting toolbar={['Delete']} editSettings={{allowDeleting: true, allowEditing: true}} width='auto'>
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (<ColumnDirective key={index} {...item} />))}
+        </ColumnsDirective>
+        <Inject services={[Page, Sort, Toolbar, Selection, Edit, Filter]} />
+      </GridComponent>
+    </div>
   )
 }
 
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -11,7 +11,7 @@ const Employees = () => {
   return (
     <div className="m-2 mt-16 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category='Page' title="Employees"/>
-      <GridComponent id="gridcomp" dataSource={employeesData} allowPaging allowSorting toolbar={['Search']} width='auto'>
+      <GridComponent dataSource={employeesData} allowPaging allowSorting toolbar={['Search']} width='auto'>
         <ColumnsDirective>
           {employeesGrid.map((item, index) => (<ColumnDirective key={index} {...item}/>))}
         </ColumnsDirective>
